a2562271a4e8b6331140c9cc6a4a57c01864a53c
Rachel Warbelow <rachelwarbelow@gmail.com>
2019-07-25 00:11:46 -0600

    alsdhfasdfds

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = express();
 const cors = require('cors');
 const faker = require('faker');
 
-database.migrate.latest([config]) 
+database.migrate.latest([configuration]) 
 
 server.set('port', process.env.PORT || 3000);
 server.use(express.json());
